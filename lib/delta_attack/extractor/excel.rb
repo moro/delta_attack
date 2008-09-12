56565775bb5583dc696d42e52eaeaca59c410f7c
moro <moronatural@gmail.com>
2008-09-13 00:10:54 +0900

    add basic extraction for MS Excel

--- lib/delta_attack/extractor/excel.rb
+++ lib/delta_attack/extractor/excel.rb
@@ -1,14 +1,42 @@
-#!/usr/bin/env ruby
+require 'java'
+
+include_class 'org.apache.poi.hssf.usermodel.HSSFWorkbook'
+include_class 'org.apache.poi.hssf.usermodel.HSSFCell'
 
 module DeltaAttack
-  class Excel
-    attr_accessor :input_stream
-    def initialize(input_stream)
-      @input_stream = input_stream
-    end
+  module Extractor
+    class Excel
+      attr_accessor :input_stream
+      def initialize(input_stream)
+        @input_stream = input_stream
+      end
 
-    def data
+      def data(ignore_cache=false)
+        return @data if !ignore_cache && @data
 
+        @data = extract_data
+      end
+
+      private
+      def extract_data
+        book = HSSFWorkbook.new(@input_stream)
+        extract_from_sheet(book.sheet_at(0))
+      end
+
+      def extract_from_sheet(sheet)
+        sheet.row_iterator.map do |row|
+          row.iterator.map do |cell|
+            case cell.cell_type
+            when HSSFCell::CELL_TYPE_NUMERIC
+              cell.numeric_cell_value
+            when HSSFCell::CELL_TYPE_STRING
+              cell.rich_string_cell_value.string
+            when HSSFCell::CELL_TYPE_BOOLEAN, HSSFCell::CELL_TYPE_BLANK
+              nil
+            end
+          end
+        end
+      end
     end
   end
 end
--- spec/extractor/excel_spec.rb
+++ spec/extractor/excel_spec.rb
@@ -1,19 +1,19 @@
-require File.expand_path("spec_helper", File.dirname(__FILE__))
-require 'delta_attack/excel'
+require File.expand_path("../spec_helper", File.dirname(__FILE__))
+require 'delta_attack/extractor/excel'
 require 'java'
 
-describe DeltaAttack::Excel do
+describe DeltaAttack::Extractor::Excel do
   include SpecHelper
   before do
     @is = Java::JavaIo::ByteArrayInputStream.new(
       File.read(sample_data("13TOKYO.xls")).to_java_bytes)
 
-    @xls = DeltaAttack::Excel.new(@is)
+    @xls = DeltaAttack::Extractor::Excel.new(@is)
   end
 
   it { @xls.input_stream.should_not be_nil }
-  it "data[0][0].should == '13101'" do
-    @xls.data[0][0].should == '13101'
+  it "data[0][0].should == 13101" do
+    @xls.data[0][0].should == 13101
   end
 end
 
