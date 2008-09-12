4c6bcbacee1376554c9635c37869fccf3c1f828d
moro <moronatural@gmail.com>
2008-09-12 18:41:10 +0900

    tiny playground for learning JRuby

--- samples/dump_xls.rb
+++ samples/dump_xls.rb
@@ -5,16 +5,15 @@ $KCODE = 'u'
 require 'java'
 require 'benchmark'
 
-include_class 'java.io.ByteArrayInputStream'
 include_class 'org.apache.poi.hssf.usermodel.HSSFWorkbook'
 include_class 'org.apache.poi.hssf.usermodel.HSSFCell'
 
-content = File.open('samples/data/tc_01.xls', "rb"){|f| f.read }
+content = ARGF.read #File.open('samples/data/tc_01.xls', "rb"){|f| f.read }
 
 puts Benchmark.realtime{
   ist = nil
   begin
-    ist = ByteArrayInputStream.new(content.to_java_bytes)
+    ist = Java::JavaIo::ByteArrayInputStream.new(content.to_java_bytes)
     book = HSSFWorkbook.new(ist)
     sheet = book.sheet_at(0)
     values = sheet.row_iterator.map do |row|
