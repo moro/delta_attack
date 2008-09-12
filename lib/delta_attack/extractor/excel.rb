b8b39af90b3e192eab694ea4e80778d0b912feae
moro <moronatural@gmail.com>
2008-09-13 00:38:15 +0900

    [refactor] extract method

--- lib/delta_attack/extractor/excel.rb
+++ lib/delta_attack/extractor/excel.rb
@@ -24,25 +24,27 @@ module DeltaAttack
         begin
           book = HSSFWorkbook.new(input_stream)
           return (0...book.number_of_sheets).map do |i|
-            extract_from_sheet(book.sheet_at(i)) 
+            extract_sheet(book.sheet_at(i))
           end
         ensure
           input_stream.close
         end
       end
 
-      def extract_from_sheet(sheet)
-        sheet.row_iterator.map do |row|
-          row.iterator.map do |cell|
-            case cell.cell_type
-            when HSSFCell::CELL_TYPE_NUMERIC
-              cell.numeric_cell_value
-            when HSSFCell::CELL_TYPE_STRING
-              cell.rich_string_cell_value.string
-            when HSSFCell::CELL_TYPE_BOOLEAN, HSSFCell::CELL_TYPE_BLANK
-              nil
-            end
-          end
+      def extract_sheet(sheet)
+        sheet.iterator.map do |row|
+          row.iterator.map{|cell| handle_cell(cell) }
+        end
+      end
+
+      def handle_cell(cell)
+        case cell.cell_type
+        when HSSFCell::CELL_TYPE_NUMERIC
+          cell.numeric_cell_value
+        when HSSFCell::CELL_TYPE_STRING
+          cell.rich_string_cell_value.string
+        when HSSFCell::CELL_TYPE_BOOLEAN, HSSFCell::CELL_TYPE_BLANK
+          nil
         end
       end
     end
