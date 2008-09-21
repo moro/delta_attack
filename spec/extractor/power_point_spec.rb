acc39128726805bbeea69da06877eed06dd3e9ce
moro <moronatural@gmail.com>
2008-09-21 19:54:22 +0900

    fix test, sample document is mine.
    (cause some warning)

--- spec/extractor/power_point_spec.rb
+++ spec/extractor/power_point_spec.rb
@@ -6,13 +6,13 @@ require 'timeout'
 describe DeltaAttack::Extractor::PowerPoint do
   include SpecHelper
   before do
-    content = File.read(sample_data("Ruby+Agile-IID.ppt"))
+    content = File.read(sample_data("named_scope06.ppt"))
     @ppt = DeltaAttack::Extractor::PowerPoint.new(content.to_java_bytes)
   end
 
   it { @ppt.bytes.should_not be_nil }
-  it "data.flatten.first.should == /Ruby\s\+\sAgile/" do
-    @ppt.data.flatten.first.should =~ /Ruby\s\+\sAgile/
+  it "data.flatten.first.should == /named_scope/" do
+    @ppt.data.flatten.first.should =~ /named_scope/
   end
 
   it "2nd call of data() should be cached" do
